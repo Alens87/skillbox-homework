c25575f291cfdc21c5f7755ca2bcb8a2d5ca603d
Alens87 <108548355+Alens87@users.noreply.github.com>
2022-07-02 00:14:31 +0300

    Create lesson2.py

---
+++ lesson2.py
@@ -0,0 +1,11 @@
+soldiers = int(input('Сколько соладат в шеренге: '))
+total_rules = int(input('Количество правил в военском уставе: '))
+push_ups = 0
+
+for soldier in range(soldiers - 4, 0, -4):
+  print('Солдат под номером -', soldier)
+  soldier_rules = int(input('Сколько правил в уставе? '))
+  if total_rules != soldier_rules:
+    print('Упал-отжался!', 10 * soldier, 'отжиманий!')
+    push_ups += 10 * soldier
+print('Всего штрафных отжиманий -', push_ups)
