86df409418d7b1ce7d0ced34428441a3f70fb16a
Sergey Tkachenko <alens1987@gmail.com>
2022-07-02 20:01:42 +0300

    изменил

--- lesson3.py
+++ lesson3.py
@@ -1,2 +1,8 @@
-print(-1/ 6)
+tax = float(input('Введите бюджет страны: '))
+new_tax = float(input('Новые поступления (налог): '))
 
+result = tax + new_tax
+if abs(tax - result) < 1e-15:
+    print('Результат: Бюджет не изменится')
+else:
+    print('Результат: Бюджет увеличится')
